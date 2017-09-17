d05147fbc82bea488b85159f2d196bdbaac148d7
okunishinishi <okunishinishi@gmail.com>
2017-09-17 13:24:04 +0900

    Use async await

--- test/define_test.js
+++ test/define_test.js
@@ -6,36 +6,35 @@
 
 const define = require('../lib/define.js')
 const ponContext = require('pon-context')
-const { ok, equal } = require('assert')
-const co = require('co')
+const {ok, equal} = require('assert')
 
 describe('define', function () {
   this.timeout(3000)
 
-  before(() => co(function * () {
+  before(async () => {
 
-  }))
+  })
 
-  after(() => co(function * () {
+  after(async () => {
 
-  }))
+  })
 
-  it('Define', () => co(function * () {
+  it('Define', async () => {
     let ctx = ponContext()
-    let { NODE_ENV } = process.env
+    let {NODE_ENV} = process.env
     process.env.NODE_ENV = 'foo'
     process.env.BAZ = 'this is baz'
-    let task = define('bar', { HOGE: 'FUGE', BAZ: 'this is baz' })
+    let task = define('bar', {HOGE: 'FUGE', BAZ: 'this is baz'})
     ok(task)
 
-    yield Promise.resolve(task(ctx))
+    await Promise.resolve(task(ctx))
     equal(process.env.NODE_ENV, 'bar')
     equal(process.env.HOGE, 'FUGE')
 
-    yield Promise.resolve(task(ctx))
+    await Promise.resolve(task(ctx))
 
     process.env.NODE_ENV = NODE_ENV
-  }))
+  })
 })
 
 /* global describe, before, after, it */
